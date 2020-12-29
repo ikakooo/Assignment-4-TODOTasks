894c934b6eddb80b9252d0d7bb9441dfb127d719
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-12-29 13:18:34 +0400

    erqsfeefer

---
+++ app/src/main/java/com/cst/todotasks/ui/BasicActivity.kt
@@ -0,0 +1,70 @@
+package com.cst.todotasks.ui
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import com.cst.todotasks.R
+import com.cst.todotasks.extensions.replaceFragment
+
+class BasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+       // replaceFragment(R.id.fragment_container, TaskListFragment.createInstance())
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            R.id.menu_clear -> {
+                // TODO თქვენი კოდი
+                true
+            }
+            R.id.menu_filter -> {
+                showFilteringPopUpMenu()
+                true
+            }
+            else -> false
+        }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.tasks_fragment_menu, menu)
+        return true
+    }
+
+    private fun showFilteringPopUpMenu() {
+        val view = findViewById<View>(R.id.menu_filter) ?: return
+        PopupMenu(this, view).run {
+            menuInflater.inflate(R.menu.filter_tasks, menu)
+
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.active -> {
+                        // TODO თქვენი კოდი
+                    }
+                    R.id.completed -> {
+                        // TODO თქვენი კოდი
+                    }
+                    else -> {
+                        // TODO თქვენი კოდი
+                    }
+                }
+                true
+            }
+            show()
+        }
+    }
+}
