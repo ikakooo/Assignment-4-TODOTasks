15cf0c003dc05ed8b67a8c72350a04a5974d82cc
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-12-29 12:53:17 +0400

    erqsfeefer

--- app/src/main/java/com/cst/todotasks/ui/MainActivity.kt
+++ app/src/main/java/com/cst/todotasks/ui/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.cst.todotasks.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import com.cst.todotasks.R
 import com.cst.todotasks.extensions.replaceFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +19,55 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
         replaceFragment(R.id.fragment_container, TaskListFragment.createInstance())
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            R.id.menu_clear -> {
+                // TODO თქვენი კოდი
+                true
+            }
+            R.id.menu_filter -> {
+                showFilteringPopUpMenu()
+                true
+            }
+            else -> false
+        }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.tasks_fragment_menu, menu)
+        return true
+    }
+
+    private fun showFilteringPopUpMenu() {
+        val view = findViewById<View>(R.id.menu_filter) ?: return
+        PopupMenu(this, view).run {
+            menuInflater.inflate(R.menu.filter_tasks, menu)
+
+            setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.active -> {
+                        // TODO თქვენი კოდი
+                    }
+                    R.id.completed -> {
+                        // TODO თქვენი კოდი
+                    }
+                    else -> {
+                        // TODO თქვენი კოდი
+                    }
+                }
+                true
+            }
+            show()
+        }
     }
 
 }
--- app/src/main/java/com/cst/todotasks/ui/TaskListFragment.kt
+++ app/src/main/java/com/cst/todotasks/ui/TaskListFragment.kt
@@ -9,57 +9,57 @@ import com.cst.todotasks.R
 /**
  * Created by nikolozakhvlediani on 12/24/20.
  */
-class TaskListFragment : Fragment() {
+class TaskListFragment : Fragment(R.layout.fragment_task_list) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val view = inflater.inflate(R.layout.fragment_task_list, container, false)
-        setHasOptionsMenu(true)
-        return view
-    }
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val view = inflater.inflate(R.layout.fragment_task_list, container, false)
+//        //setHasOptionsMenu(true)
+//        return view
+//    }
 
-    override fun onOptionsItemSelected(item: MenuItem) =
-        when (item.itemId) {
-            R.id.menu_clear -> {
-                // TODO თქვენი კოდი
-                true
-            }
-            R.id.menu_filter -> {
-                showFilteringPopUpMenu()
-                true
-            }
-            else -> false
-        }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.tasks_fragment_menu, menu)
-    }
-
-    private fun showFilteringPopUpMenu() {
-        val view = activity?.findViewById<View>(R.id.menu_filter) ?: return
-        PopupMenu(requireContext(), view).run {
-            menuInflater.inflate(R.menu.filter_tasks, menu)
-
-            setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.active -> {
-                        // TODO თქვენი კოდი
-                    }
-                    R.id.completed -> {
-                        // TODO თქვენი კოდი
-                    }
-                    else -> {
-                        // TODO თქვენი კოდი
-                    }
-                }
-                true
-            }
-            show()
-        }
-    }
+//    override fun onOptionsItemSelected(item: MenuItem) =
+//        when (item.itemId) {
+//            R.id.menu_clear -> {
+//                // TODO თქვენი კოდი
+//                true
+//            }
+//            R.id.menu_filter -> {
+//                showFilteringPopUpMenu()
+//                true
+//            }
+//            else -> false
+//        }
+//
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        inflater.inflate(R.menu.tasks_fragment_menu, menu)
+//    }
+//
+//    private fun showFilteringPopUpMenu() {
+//        val view = activity?.findViewById<View>(R.id.menu_filter) ?: return
+//        PopupMenu(requireContext(), view).run {
+//            menuInflater.inflate(R.menu.filter_tasks, menu)
+//
+//            setOnMenuItemClickListener {
+//                when (it.itemId) {
+//                    R.id.active -> {
+//                        // TODO თქვენი კოდი
+//                    }
+//                    R.id.completed -> {
+//                        // TODO თქვენი კოდი
+//                    }
+//                    else -> {
+//                        // TODO თქვენი კოდი
+//                    }
+//                }
+//                true
+//            }
+//            show()
+//        }
+//    }
 
     companion object {
 
