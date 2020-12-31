939a08617b6df4d6085ca09044d626ce7008ef4d
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-01-01 02:08:47 +0400

    sdfsfdsf

---
+++ app/src/main/java/com/cst/todotasks/Constants.kt
@@ -0,0 +1,5 @@
+package com.cst.todotasks
+
+object Constants {
+    const val firstFragmentPath = "Destination(com.cst.todotasks:id/FirstFragment) label=First Fragment class=com.cst.todotasks.ui.FirstFragment"
+}
--- app/src/main/java/com/cst/todotasks/ui/BasicActivity.kt
+++ app/src/main/java/com/cst/todotasks/ui/BasicActivity.kt
@@ -1,28 +1,67 @@
 package com.cst.todotasks.ui
 
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.PopupMenu
+import androidx.navigation.findNavController
+import androidx.viewbinding.ViewBinding
+import com.cst.todotasks.Constants.firstFragmentPath
 import com.cst.todotasks.R
-import com.cst.todotasks.extensions.replaceFragment
+import com.cst.todotasks.databinding.ActivityMainBinding
 
 class BasicActivity : AppCompatActivity() {
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-       // replaceFragment(R.id.fragment_container, TaskListFragment.createInstance())
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        // replaceFragment(R.id.fragment_container, TaskListFragment.createInstance())
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
+        binding.fab.apply {
+            setOnClickListener {
+//                setImageDrawable(AppCompatResources.getDrawable(context, R.drawable.ic_edit))
+                if (firstFragmentPath == navController.currentDestination.toString()) {
+                    binding.toolbar.title = "ikakk"
+                    setImageDrawable(AppCompatResources.getDrawable(context, R.drawable.ic_edit))
+                    navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+                } else {
+                    setImageDrawable(AppCompatResources.getDrawable(context, R.drawable.ic_add))
+//                    setBackgroundResource(R.drawable.ic_add)
+                    navController.navigate(R.id.action_SecondFragment_to_FirstFragment)
+
+
+                }
+
+            }
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     override fun onOptionsItemSelected(item: MenuItem) =
@@ -66,5 +105,11 @@ class BasicActivity : AppCompatActivity() {
             }
             show()
         }
+    }
+
+    private inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+        crossinline bindingInflater: (LayoutInflater) -> T
+    ) = lazy(LazyThreadSafetyMode.NONE) {
+        bindingInflater.invoke(layoutInflater)
     }
 }
